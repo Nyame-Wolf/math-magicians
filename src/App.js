6ad83b0a462b8292616ccc3e6a66e33f00b9c158
Nyame-Wolf <mumenyam@gmail.com>
2022-08-12 12:22:48 +0300

    Add components testing by testing snapshot
    Add logic testing for the calculate and operate
    
    Add test to ensure component renders

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Calculator from './components/Calculator';
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import Quotes from './pages/Quotes';
 
 const App = () => (
-  <>
+
+  <div>
     <div className="heading">
       <h1>Math Magicians</h1>
       <Navigation />
     </div>
-
-    <Outlet />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="quotes" element={<Quotes />} />
+    </Routes>
+    {/* <Outlet /> */}
     <Footer />
-  </>
+  </div>
 );
 
 export default App;
