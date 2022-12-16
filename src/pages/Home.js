22498862aff5d45b1ff1d5d60b88a78b0fdc4aad
Nyame-Wolf <mumenyam@gmail.com>
2022-12-16 13:45:00 +0300

    add image and fix header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,25 @@
+import img from '../assets/images/mathsimage.jpeg';
+
 const Home = () => (
-  <>
-    <p className="info">
-      Nisi cillum exercitation cillum proident
-      occaecat. Exercitation nisi aliquip irure id
-      nostrud aliqua. Dolor aliquip ut
-      occaecat duis labore laborum labore exercitation.
-      Quis et non consequat id cupidatat sunt ut.
-      Reprehenderit cupidatat ipsum est nisi minim culpa irure.
-      Cillum ullamco anim sit est eiusmod et et veniam.
-      Ex mollit sit amet adipisicing do in ex mollit ad.
-    </p>
-    <p className="info">
-      Nisi cillum exercitation cillum proident
-      occaecat. Exercitation nisi aliquip irure id
-      nostrud aliqua. Dolor aliquip ut
-      occaecat duis labore laborum labore exercitation.
-      Quis et non consequat id cupidatat sunt ut.
-      Reprehenderit cupidatat ipsum est nisi minim culpa irure.
-      Cillum ullamco anim sit est eiusmod et et veniam.
-      Ex mollit sit amet adipisicing do in ex mollit ad.
-    </p>
-  </>
+  <div className="home">
+    <img className="image" src={img} alt="bkg-pic" />
+    <div>
+      <p className="info">
+        Mathematics as an expression of the human mind reflects
+        the active will, the contemplative reason, and the desire
+        for aesthetic perfection. Its basic elements are logic and
+        intuition, analysis and construction, generality and individuality.
+        — Richard Courant, German-American mathematician
+      </p>
+      <p className="info">
+        Mathematics are the result of mysterious powers which no one understands
+        , and which the unconscious recognition of beauty must play an important
+        part. Out of an infinity of designs a mathematician chooses one pattern for
+        beauty’s sake and pulls it down to earth.
+        — Marston Morse, American mathematician
+      </p>
+    </div>
+  </div>
 );
 
 export default Home;
